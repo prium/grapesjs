bdcbe3f951fd72a1505ad5143bbded1dd92e5d45
Ashraful Prium <877255+prium@users.noreply.github.com>
2025-12-08 20:39:52 -0800

    add srcdoc in canvas frame

--- packages/core/src/canvas/view/FrameView.ts
+++ packages/core/src/canvas/view/FrameView.ts
@@ -21,7 +21,7 @@ export default class FrameView extends ModuleView<Frame, HTMLIFrameElement> {
   }
   /** @ts-ignore */
   get attributes() {
-    return { allowfullscreen: 'allowfullscreen' };
+    return { allowfullscreen: 'allowfullscreen', srcdoc: 'canvas' }; //srcdoc a patch to make service workers work
   }
 
   dragging = false;
